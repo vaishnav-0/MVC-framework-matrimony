299d53441f16dea99473a00bea8f2db807bad5fd
Anugeeth <anugeeth2@gmail.com>
2020-12-23 02:41:36 +0530

    updated routes and added contact controller

---
+++ App/Controller/contact.php
@@ -0,0 +1,27 @@
+<?php
+namespace Matr\Controller;
+use Matr\Model\dbModel\contactModel;
+use Core\Request;
+use Core\Response;
+class contact{
+    
+    public static function getContact(Request $req, Response $res){
+  
+
+    }
+    public static function editContact(Request $req, Response $res){
+ 
+
+    }
+    public static function addContact(Request $req, Response $res){
+
+
+    }
+    public static function deleteContact(Request $req, Response $res){
+
+
+    }
+
+}
+
+?>
--- core/Routes.php
+++ core/Routes.php
@@ -3,5 +3,13 @@ namespace Core;
 class Routes extends Router{
     public static function setRoutes(){                             // set your routes here
         Router::get("auth","Matr\\Controller\\auth::testfunc");
+        Router::get("contact","Matr\\Controller\\contact::testfunc");
+
+        // contact routes
+        Router::get("contact","Matr\\Controller\\contact::getContact");
+        Router::post("contact","Matr\\Controller\\contact::addContact");
+        Router::patch("contact","Matr\\Controller\\contact::editContact");
+        Router::delete("contact","Matr\\Controller\\contact::deleteContact");
+
     }
 }
