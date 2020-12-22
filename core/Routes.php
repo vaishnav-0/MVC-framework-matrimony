26b24f5b30c5cbbc720b8765afb6f2402917b27d
vaichu <vaishnavvaichu123@gmail.com>
2020-12-23 02:40:15 +0530

    .

--- core/Routes.php
+++ core/Routes.php
@@ -2,6 +2,6 @@
 namespace Core;
 class Routes extends Router{
     public static function setRoutes(){                             // set your routes here
-        Router::get("auth","Matr\\Controller\\auth::testfunc");
+        Router::get("abcd","Matr\\Controller\\auth::testfunc");
     }
 }
