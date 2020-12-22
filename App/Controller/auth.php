d846ef741c6601d425aff9711170e05ce6b45f07
vaichu <vaishnavvaichu123@gmail.com>
2020-12-23 01:18:07 +0530

    .

--- App/Controller/auth.php
+++ App/Controller/auth.php
@@ -6,7 +6,11 @@ use Core\Response;
 class auth{
     
     public static function testfunc(Request $req, Response $res){
-        $res->json((object)["message"=>$req->body->msg]);
+        if($req->body->msg)
+            $res->json((object)["messages"=>$req->body->msg]);
+        else 
+            $res->json((object)["messages"=>"No messages"]);
+
     }
 
 }
