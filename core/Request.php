c2936ab2b6c471208d635d86af9e13e53c4d2db0
vaichu <vaishnavvaichu123@gmail.com>
2020-12-23 02:04:31 +0530

    .

--- core/Request.php
+++ core/Request.php
@@ -15,7 +15,7 @@ class Request
     
     public function strip()
     {   
-        if (!function_exists('array_key_first')) {
+        if (!function_exists('array_key_first')) { // polyfill for array_key_first
             function array_key_first(array $arr) {
                 foreach($arr as $key => $unused) {
                     return $key;
