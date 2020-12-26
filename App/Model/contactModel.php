f8ae08db5cf70a53bdc2511ebe754f51c486e1ad
vaichu <vaishnavvaichu123@gmail.com>
2020-12-27 01:57:25 +0530

    .

--- App/Model/contactModel.php
+++ App/Model/contactModel.php
@@ -2,60 +2,60 @@
 namespace Matr\Model;
 class contactModel{
     public $con;
-    function __construct($conn){
+    function __construct($conn)
+    {
+ 
         $this->con = $conn;
+
     }
     
-    public function getContact($id){
-        $exec =  $this->con->executeStatement($queryBuilder
-        ->select('*')
-        ->from('contact_details')
-        ->where('contact_id',$id)
-        );  
-        if(!$exec)  
-            return true;
-        else
-            return false;    
+    public function getContact(){
+        $stmp = $this->con->prepare("");
+        $stmp->bind_param("issi",);
+        if ($stmp->execute()){
+            $this->t = $this->returnId();
+        }
+        else{
+            return false;
+        }
+        $stmp->close();
+        
     }
 
     public function addContact($mob,$mail,$landline){
-        $exec = $this->con->executeStatement($this->con->queryBuilder->insert('contact_details')->values(
+        return $this->con->executeStatement($this->con->queryBuilder->insert('contact_details')->values(
             array(
                 'mobile_no' => '?',
                 'mail_id' => '?',
                 'landline' => '?'
             )
         ), array($mob,$mail,$landline));
-        if(!$exec)  
-            return true;
-        else
-            return false;
     }
 
-    public function editContact($id,$mob,$mail,$landline){
-        $exec =  $this->con->executeStatement($queryBuilder
-            ->update('contact_details')
-            ->set('mobile_no',$mob )
-            ->set('mail_id', $mail)
-            ->set('landline',$landline)
-            ->where('contact_id' ,$id)
-        );
-        if(!$exec)  
-            return true;
-        else
+    public function editContact(){
+        $stmp = $this->con->prepare("");
+        $stmp->bind_param("issi",);
+        if ($stmp->execute()){
+            $this->t = $this->returnId();
+        }
+        else{
             return false;
+        }
+        $stmp->close();
+        
     }
 
-    private function deleteContact($id){
-        $exec =  $this->con->executeStatement($queryBuilder
-            ->delete('contact_details')
-            ->where('contact_id',$id)
-        );    
-        if(!$exec)  
-            return true;
-        else
-            return false;    
+    private function deleteContact(){
+        $s = $this->con->prepare("");
+        $s->bind_param("s",$this->cust_id);
+        $s->execute();
+		$res = $s->get_result();
+        $s->close();
+        $res_array = $res->fetch_assoc();
+        return $res_array['#'];
+        
     }
 
 }
+
 ?>
