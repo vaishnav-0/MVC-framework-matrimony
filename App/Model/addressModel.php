d9337982a789a36c08fa78bfdfdb4ba389f4d606
Anugeeth <anugeeth2@gmail.com>
2020-12-28 17:22:20 +0530

    added sibling model and address model

---
+++ App/Model/addressModel.php
@@ -0,0 +1,56 @@
+<?php
+namespace Matr\Model;
+class addressModel{
+    public $con;
+    public $queryBuilder;
+    function __construct($conn)
+    {
+        $this->con = $conn;
+        $this->queryBuilder = $conn->createQueryBuilder();
+    }
+    
+    public function getAddress($id){
+        return $this->con->executeQuery($this->queryBuilder
+        ->select('*')
+        ->from('address')
+        ->where('a_id = ?'),array($id)
+        )->fetchAllAssociative();  
+    }
+
+    public function addAddress($address,$locality,$city,$district,$pin,$landmark){
+        return $this->con->executeStatement($this->queryBuilder->insert('address')->values(
+            array(
+                'address' => '?',	
+                'locality'  => '?',	
+                'city'	 => '?',
+                'district'	 => '?',
+                'pin'	 => '?',
+                'landmark'	 => '?',
+                )
+        ), array($address,$locality,$city,$district,$pin,$landmark));
+    }
+
+    public function editAddress($a_id,$address,$locality,$city,$district,$pin,$landmark){
+        return $this->con->executeStatement($this->queryBuilder
+            ->update('address')
+            ->set('city',$city )
+            ->set('district',$district )
+            ->set('address', $address)
+            ->set('locality',$locality)
+            ->set('pin',$pin)
+            ->set('landmark',$landmark)
+            ->where('a_id' ,$id)
+        );
+
+    }
+
+    private function deleteAddress($id){
+        return $this->con->executeStatement($this->queryBuilder
+            ->delete('address')
+            ->where('a_id',$id)
+        );      
+    }
+
+}
+
+?>
---
+++ App/Model/siblingModel.php
@@ -0,0 +1,54 @@
+<?php
+namespace Matr\Model;
+class siblingModel{
+    public $con;
+    public $queryBuilder;
+    function __construct($conn)
+    {
+        $this->con = $conn;
+        $this->queryBuilder = $conn->createQueryBuilder();
+    }
+    
+    public function getSibling($id){
+        return $this->con->executeQuery($this->queryBuilder
+        ->select('*')
+        ->from('sibling')
+        ->where('s_id = ?'),array($id)
+        )->fetchAllAssociative();  
+    }
+
+    public function addSibling($f_id,$name,$age,$sex,$marital_status){
+        return $this->con->executeStatement($this->queryBuilder->insert('sibling')->values(
+            array(
+                'f_id' => '?',	
+                'name'  => '?',	
+                'age'	 => '?',
+                'sex'	 => '?',
+                'marital_status'	 => '?'
+                )
+        ), array($f_id,$name,$age,$sex,$marital_status));
+    }
+
+    public function editSibling($s_id,$f_id	,$name,$age,$sex,$marital_status){
+        return $this->con->executeStatement($this->queryBuilder
+            ->update('sibling')
+            ->set('f_id',$f_id )
+            ->set('name', $name)
+            ->set('age',$age)
+            ->set('sex',$sex)
+            ->set('marital_status',$marital_status)
+            ->where('s_id' ,$id)
+        );
+
+    }
+
+    private function deleteSibling($id){
+        return $this->con->executeStatement($this->queryBuilder
+            ->delete('sibling')
+            ->where('s_id',$id)
+        );      
+    }
+
+}
+
+?>
