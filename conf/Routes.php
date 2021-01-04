c2a7940631d6ce2d6e2d21d7cc47d994238efa14
vaichu <vaishnavvaichu123@gmail.com>
2021-01-04 15:25:24 +0530

    .

--- conf/Routes.php
+++ conf/Routes.php
@@ -1,30 +1,40 @@
 <?php
 namespace Conf;
+
 use Core\Router;
 use Matr\Controller\Auth;
 use Matr\Controller\Contact;
 use Matr\Controller\Member;
-class Routes {
-    public static function setRoutes(Router $router){   
+
+class Routes
+{
+    public static function setRoutes(Router $router)
+    {
         // set your routes here
 
         //authentication routes
-        $router->post("login",[Auth::class,"login"]);
-        $router->get("logout",[Auth::class,"logout"]);
-        $router->post("register",[Auth::class,"register"]);
+        $router->post("login", [Auth::class,"login"]);
+        $router->get("logout", [Auth::class,"logout"]);
+        $router->post("register", [Auth::class,"register"]);
 
         // member routes
-        $router->get("member",[Member::class,"get"]);
-        $router->get("member/all",[Member::class,"getAll"]);
-        $router->post("member",[Member::class,"add"]);
-        $router->patch("member",[Member::class,"edit"]);
-        $router->delete("member",[Member::class,"delete"]);
-
+        $router->get("member", [Member::class,"get"]);
+        $router->get("member/all", [Member::class,"getAll"]);
+        $router->post("member", [Member::class,"add"]);
+        $router->patch("member", [Member::class,"edit"]);
+        $router->delete("member", [Member::class,"delete"]);
+        
+        // family routes
+        $router->get("family", [Family::class,"get"]);
+        $router->post("family", [Family::class,"add"]);
+        $router->patch("family", [Family::class,"edit"]);
+        $router->delete("family", [Family::class,"delete"]);
+        
         // contact routes
-        $router->get("contact",[Contact::class,"get"]);
-        $router->post("contact",[Contact::class,"add"]);
-        $router->patch("contact",[Contact::class,"edit"]);
-        $router->delete("contact",[Contact::class,"delete"]);
+        $router->get("contact", [Contact::class,"get"]);
+        $router->post("contact", [Contact::class,"add"]);
+        $router->patch("contact", [Contact::class,"edit"]);
+        $router->delete("contact", [Contact::class,"delete"]);
 
         return $router;
     }
