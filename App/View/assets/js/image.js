2c61c5e47dad82f60cff0685b11ebc1e5853d0d6
Anugeeth <anugeeth2@gmail.com>
2021-01-13 21:42:23 +0530

    .

---
+++ App/View/assets/js/image.js
@@ -0,0 +1,19 @@
+const fileSelect = document.querySelector(".profilePhoto");
+const fileElem = document.querySelector("#profileUp");
+fileSelect.addEventListener("click", function (e) {
+    if (fileElem) {
+        fileElem.click();
+    }
+}, false);
+const inputElement = document.getElementById("profileUp");
+inputElement.addEventListener("change", handleFile, false);
+
+export function handleFile() {
+    const file = this.files[0];
+    if (file.type.startsWith('image/')) {
+        const img = document.getElementById("profilePic");
+        img.file = file;
+        const objectURL = window.URL.createObjectURL(file);
+        img.src = objectURL;
+    }
+}
